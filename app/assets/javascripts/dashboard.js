b05ba0cfc56d3233078236967bb5eaabb58fa99b
VitorGuerra <vitorhugoguerraguimaraes@gmail.com>
2025-06-05 15:34:13 -0300

    cofigurando container de exibição

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,28 @@
+// app/assets/javascripts/dashboard.js
+
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleButton = document.getElementById('toggle-balance-button');
+  const currentBalanceElement = document.getElementById('current-balance');
+  
+  const actualBalanceText = currentBalanceElement.innerHTML; 
+  const maskedBalanceText = "Total em conta: ******";
+
+  // Oculta o saldo na inicialização e
+  currentBalanceElement.innerHTML = maskedBalanceText; 
+  currentBalanceElement.style.display = 'block';
+  toggleButton.textContent = 'Ver Saldo';
+
+  if (toggleButton && currentBalanceElement) {
+    toggleButton.addEventListener('click', function() {
+      // Se o saldo atual for o ****, mostra o real
+      if (currentBalanceElement.innerHTML === maskedBalanceText) {
+        currentBalanceElement.innerHTML = actualBalanceText; 
+        toggleButton.textContent = 'Ocultar Saldo'; 
+      } else {
+        // Se o saldo atual for o real, mostra o ****
+        currentBalanceElement.innerHTML = maskedBalanceText; 
+        toggleButton.textContent = 'Ver Saldo';
+      }
+    });
+  }
+});

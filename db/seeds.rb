1e03cbcbc48ee609868ab9e49b7365f7a043de6b
VitorGuerra <vitorhugoguerraguimaraes@gmail.com>
2025-06-06 16:20:20 -0300

    ajustes de população no banco e docker configs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!([
-  { username: "12345", password: "1234", balance: 1000.0, vip: false },
-  { username: "54321", password: "1234", balance: 2000.0, vip: true },
-  { username: "11111", password: "1234", balance: 500.0, vip: false }
+  { username: "12345", password: "1234", balance: 1000, vip: false },
+  { username: "54321", password: "1234", balance: 2000, vip: true },
+  { username: "11111", password: "1234", balance: 500, vip: false }
 ])
